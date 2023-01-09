dcaa306b9d030252cf79124b89921db16a9d4a14
atizaz786 <your_email@example.com>
2023-01-09 17:03:49 +0000

    feat: changed the theme of app

--- src/pages/socialEngineering.jsx
+++ src/pages/socialEngineering.jsx
@@ -15,6 +15,9 @@ import {
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(3, 2),
+    background: 'rgb(36,36,113)',
+      background: 'linear-gradient(243deg, rgba(36,36,113,1) 13%, rgba(0,0,0,1) 43%, rgba(5,0,83,1) 79%)',
+      color:"#fff"
   },
   title: {
     marginBottom: theme.spacing(2),
--- src/pages/training.jsx
+++ src/pages/training.jsx
@@ -1,19 +1,44 @@
 import React,{useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { AppBar, Toolbar, Typography, Drawer, List, ListItem, ListItemText } from '@material-ui/core';
 import {Introduction, PasswordSecurity, RecapAndNextSteps,MobileSecurity, SocialEngineering, EmailAndPhishingAttacks, SafeBrowsing} from './functions'
 import CssBaseline from '@material-ui/core/CssBaseline';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    background: 'rgb(36,36,113)',
+      background: 'linear-gradient(243deg, rgba(36,36,113,1) 13%, rgba(0,0,0,1) 43%, rgba(5,0,83,1) 79%)',
+      color:"#fff"
   },
   appBar: {
+    background: 'rgb(36,36,113)',
+      background: 'linear-gradient(243deg, rgba(36,36,113,1) 13%, rgba(0,0,0,1) 43%, rgba(5,0,83,1) 79%)',
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
-    textAlign: 'center',
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
   },
   drawer: {
     width: drawerWidth,
@@ -21,13 +46,35 @@ const useStyles = makeStyles((theme) => ({
   },
   drawerPaper: {
     width: drawerWidth,
+    background: 'rgb(36,36,113)',
+      background: 'linear-gradient(243deg, rgba(36,36,113,1) 13%, rgba(0,0,0,1) 43%, rgba(5,0,83,1) 79%)',
+      color:"#fff"
   },
-  // necessary for content to be below app bar
-  toolbar: theme.mixins.toolbar,
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
   content: {
+    background: 'rgb(36,36,113)',
+      background: 'linear-gradient(243deg, rgba(36,36,113,1) 13%, rgba(0,0,0,1) 43%, rgba(5,0,83,1) 79%)',
     flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
     padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
 }));
 
@@ -35,6 +82,16 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Training() {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   const [page, setPage] = useState('Introduction');
 
   function handleMenuItemClick(page) {
@@ -44,22 +101,39 @@ export default function Training() {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
+      <AppBar  position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}>
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Kale University Cybersecurity Awareness Training
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Cyber Awareness Training
           </Typography>
         </Toolbar>
       </AppBar>
       <Drawer
         className={classes.drawer}
-        variant="permanent"
+        variant="persistent"
+        anchor="left"
+        open={open}
         classes={{
           paper: classes.drawerPaper,
         }}
-        anchor="left"
       >
-         <div className={classes.toolbar} />
+         <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
         <List>
           <ListItem button onClick={() => handleMenuItemClick('Introduction')}>
             <ListItemText primary="Introduction" />
@@ -84,8 +158,11 @@ export default function Training() {
           </ListItem>
         </List>
       </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
+      <main style={{background: 'rgb(36,36,113)',
+      background: 'linear-gradient(243deg, rgba(36,36,113,1) 13%, rgba(0,0,0,1) 43%, rgba(5,0,83,1) 79%)'}}  className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}>
+        <div className={classes.drawerHeader} />
         {page === 'Introduction' && <Introduction />}
       {page === 'Password Security' && <PasswordSecurity />}
       {page === 'Email and Phishing Attacks' && <EmailAndPhishingAttacks />}
